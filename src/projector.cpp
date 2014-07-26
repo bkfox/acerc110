21882df25e7e81bb299c57a5b341db44525a62d1
lordblackfox <me lordblackfox net>
2014-07-26 12:04:44 +0200

    update copyright notice (automatic)

--- src/projector.cpp
+++ src/projector.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2012 - Thomas Baquet (aka lordblackfox)
+Copyright (C) 2012 - Thomas Baquet 
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
